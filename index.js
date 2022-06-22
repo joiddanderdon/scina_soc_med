fbbbcdbab2b27c53937dcbc61aecb848ff5c44cd
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2022-06-22 08:27:08 -0400

    Revert "Books Demo"
    This reverts commit 67a5d5166167f809f367d858f79bc445011f5dec.

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.use('/post', postRoutes);
 app.use('/comment', commentRoutes);
 
 //create port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
